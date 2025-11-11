06e39c02677bd02ded41299c3524c395f75af86d
Practicals <karanvisani719@gmail.com>
2025-11-11 23:57:41 +0530

    login Signup Complete

---
+++ app1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('login/',views.login_page),
+    path('signup/',views.signup_page)
+]
---
+++ app1/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpRequest
+from django.contrib.auth.hashers import make_password, check_password
+from .models import User, Role
+
+
+# Create your views here.
+def login_page(request :HttpRequest):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    return login(request)
+
+def login(request :HttpRequest):
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+
+    if email is None or password is None:
+        return HttpResponse("Email and Password are compulsory")
+    
+    user = User.objects.filter(email=email).first()
+    if user is None:
+        return HttpResponse("Wrong Email or Password")
+    
+    return HttpResponse("Logged in Successfully")
+
+
+def signup_page(request :HttpRequest):
+    if request.method == 'GET':
+        return render(request,'signup.html')
+    return signup(request)
+
+def signup(request :HttpRequest):
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+
+    if email is None or password is None :
+        return HttpResponse("Email and Password are compulsory")
+    
+    if User.objects.filter(email=email).exists():
+        return HttpResponse("Sorry, this email is not available")
+    
+    customer_role = Role.objects.get(name="Customer")
+
+    user = User()
+    user.email = email
+    user.password_hash = make_password(password)
+    user.role = customer_role
+    user.save()
+
+    return HttpResponse("Successfully Signed Up") 
